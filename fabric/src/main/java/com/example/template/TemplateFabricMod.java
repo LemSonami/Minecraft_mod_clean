b83131028c7420dbec4bfdaa3c321ce6cbef1363
LemSonami <1409522639@qq.com>
2026-06-21 22:55:46 +0800

    first commit

---
+++ fabric/src/main/java/com/example/template/TemplateFabricMod.java
@@ -0,0 +1,10 @@
+package com.example.template;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TemplateFabricMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Fabric mod entry point
+    }
+}
---
+++ forge/src/main/java/com/example/template/TemplateForgeMod.java
@@ -0,0 +1,10 @@
+package com.example.template;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("sonami_mod")
+public class TemplateForgeMod {
+    public TemplateForgeMod() {
+        // Forge mod entry point
+    }
+}
